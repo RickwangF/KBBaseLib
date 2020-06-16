dc284a5e28944e099ae75156384b0103a9027707
woshiwwy16@126.com <Wwy-+901116>
2020-06-16 16:29:24 +0800

    add screen shot method

--- kbbaselib/src/main/java/com/kbit/kbbaselib/util/DisplayUtil.java
+++ kbbaselib/src/main/java/com/kbit/kbbaselib/util/DisplayUtil.java
@@ -1,10 +1,17 @@
 package com.kbit.kbbaselib.util;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Environment;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.kbit.kbbaselib.context.ContextProvider;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class DisplayUtil {
 
@@ -50,6 +57,32 @@ public class DisplayUtil {
         assert windowManager != null;
         windowManager.getDefaultDisplay().getMetrics(displayMetrics);
         return displayMetrics;
+    }
+
+    public static String screenshot(Activity context) {
+        // 获取屏幕
+        String path = "";
+        View dView = context.getWindow().getDecorView();
+        dView.setDrawingCacheEnabled(true);
+        dView.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(dView.getDrawingCache());
+        if (bitmap != null) {
+            try {
+                // 获取内置SD卡路径
+                String sdCardPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath();
+                // 图片文件路径
+                path = sdCardPath + File.separator + System.currentTimeMillis() + ".jpg";
+
+                File file = new File(path);
+                FileOutputStream os = new FileOutputStream(file);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 80, os);
+                os.flush();
+                os.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return path;
     }
 
 }
